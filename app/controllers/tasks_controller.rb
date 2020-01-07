054a528820b34b2aaf0a7ce14a88be8bf5c07ac2
takahiro masuda <taka.baseball.0306@icloud.com>
2020-01-07 09:10:39 +0000

    add amendment to tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,19 +1,20 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in, only: [:index, :show]
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :set_task, only: [:show, :edit, :update]
+  before_action :correct_user, only: [:destroy]
   
   
   def index
-    @tasks = Task.order(id: :desc).page(params[:page]).per(20)
+    @tasks = current_user.tasks.order(id: :desc).page(params[:page]).per(20)
   end
   def show
     
   end
   def new 
-    @task = Task.new
+    @task = current_user.tasks.build
   end
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     if @task.save
       flash[:success] = 'タスクが正常に作成されました'
       redirect_to @task
@@ -35,21 +36,27 @@ class TasksController < ApplicationController
     end
   end
   def destroy
-    
     @task.destroy
     
     flash[:success] = 'タスクは正常に削除されました'
-    redirect_to tasks_url
+    redirect_to root_url
   end
   
   private 
   
   def set_task
-    @task = Task.find(params[:id])
+    @task = current_user.tasks.find(params[:id])
   end
   
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
  
--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,6 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   resources :users
   
-  resources :tasks
+  get 'delete', to: 'tasks#destroy'
+  resources :tasks 
 end
