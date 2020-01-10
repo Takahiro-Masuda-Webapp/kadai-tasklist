19e7a672b6835d7c06188e90e78f9e1292f19615
takahiro masuda <taka.baseball.0306@icloud.com>
2020-01-10 18:41:12 +0000

    add favorite-file

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_secure_password
   
   has_many :tasks
+  has_mamy :favorites
   
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 },
