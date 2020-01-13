4fe2136cec2c17da43bdf220a556d21b8ee9cf12
takahiro masuda <taka.baseball.0306@icloud.com>
2020-01-13 08:54:01 +0000

    remove sessions#router

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
   root to: 'tasks#index'
   
   get 'login', to: 'sessions#new'
@@ -10,7 +7,7 @@ Rails.application.routes.draw do
   get 'logout', to: 'sessions#destroy'
   
   get 'signup', to: 'users#new'
-  resources :users, only: [:new, :create]
+  resources :users, only: [:create]
   
   get 'delete', to: 'tasks#destroy'
   resources :tasks 
